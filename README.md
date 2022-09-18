68c28e3ba9690b44c342e0815abcbe7086d8d0e3
root <root@Corys-Desktop.localdomain>
2022-09-18 00:23:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple hello world app
+## this repository is a simple demo to explain how to push a flask project to github.com# FSDI_111
--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return"<h1>Hello, world</h1>"
+
+@app.route("/products")
+def products():
+    product_list = ["apples", "oranges", "bananas"]
+    bullet_list = "".join(
+        "<li>%s</li>" % product for product in product_list
+        )
+    return "<ul>%s</ul>" % bullet_list
