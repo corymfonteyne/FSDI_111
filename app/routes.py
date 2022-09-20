1aaeba218cd3e5771e021042886303f180ba4dff
Cory Fonteyne <corymfonteyne@gmail.com>
2022-09-20 00:52:07 -0400

    111 class 2

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html")
+
+@app.get("/about")
+def about():
+    me = {
+        "first_name": "Cory",
+        "last_name": "Fonteyne",
+        "hobbies": "woodworking",
+        "bio": "My name is Cory Fonteyne and I like turtles"
+    }
+    return render_template("about.html", about_dict=me)
+
+@app.get("/objects")
+def objects():
+    return render_template("objects.html")
